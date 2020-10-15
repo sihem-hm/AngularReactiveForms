20034129b78a6f95b580e33a54e8bd34d323a536
sihem-hm <hammamisihem6@gmail.com>
2020-10-15 11:38:07 +0100

    dynamically-duplicate-input-elements

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn, FormArray } from '@angular/forms';
+
 import 'rxjs/add/operator/debounceTime';
-import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+
 import { Customer } from './customer';
 function emailMatcher(c: AbstractControl): {[key: string]: boolean} | null {
   let emailControl = c.get('email');
@@ -37,62 +40,79 @@ export class ReactiveFormsComponent{
     pattern: 'Please enter a valid email address.',
     minlength: 'Please enter at least 4 characters.'
 };
+get addresses(): FormArray{
+  return <FormArray>this.customerForm.get('addresses');
+}
 
 
 constructor(private fb: FormBuilder) { }
 
-    ngOnInit(): void {
-        this.customerForm = this.fb.group({
-            firstName: ['', [Validators.required, Validators.minLength(3)]],
-            lastName: ['', [Validators.required, Validators.maxLength(50)]],
-            emailGroup: this.fb.group({
-                email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+'),
-                            Validators.minLength(4)]],
-                confirmEmail: ['', Validators.required],
-            }, {validator: emailMatcher}),
-            phone: '',
-            notification: 'email',
-            rating: ['', ratingRange(1, 5)],
-            sendCatalog: true
-        });
+ngOnInit(): void {
+    this.customerForm = this.fb.group({
+        firstName: ['', [Validators.required, Validators.minLength(3)]],
+        lastName: ['', [Validators.required, Validators.maxLength(50)]],
+        emailGroup: this.fb.group({
+            email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+')]],
+            confirmEmail: ['', Validators.required],
+        }, {validator: emailMatcher}),
+        phone: '',
+        notification: 'email',
+        rating: ['', ratingRange(1, 5)],
+        sendCatalog: true,
+        addresses: this.fb.array([ this.buildAddress() ])
+    });
 
-        this.customerForm.get('notification').valueChanges
-                         .subscribe(value => this.setNotification(value));
+    this.customerForm.get('notification').valueChanges
+                     .subscribe(value => this.setNotification(value));
 
-        const emailControl = this.customerForm.get('emailGroup.email');
-        emailControl.valueChanges.debounceTime(1000).subscribe(value =>
-            this.setMessage(emailControl));
+    const emailControl = this.customerForm.get('emailGroup.email');
+    emailControl.valueChanges.debounceTime(1000).subscribe(value =>
+        this.setMessage(emailControl));
+}
+
+addAddress(): void {
+    this.addresses.push(this.buildAddress());
+}
+
+buildAddress(): FormGroup {
+    return this.fb.group({
+            addressType: 'home',
+            street1: '',
+            street2: '',
+            city: '',
+            state: '',
+            zip: ''
+    });
+}
+
+populateTestData(): void {
+    this.customerForm.patchValue({
+        firstName: 'Jack',
+        lastName: 'Harkness',
+        sendCatalog: false
+    });
+}
+
+save(): void {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+}
+
+setMessage(c: AbstractControl): void {
+    this.emailMessage = '';
+    if ((c.touched || c.dirty) && c.errors) {
+        this.emailMessage = Object.keys(c.errors).map(key =>
+            this.validationMessages[key]).join(' ');
     }
+}
 
-    populateTestData(): void {
-        this.customerForm.patchValue({
-            firstName: 'Jack',
-            lastName: 'Harkness',
-            sendCatalog: false
-        });
+setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVia === 'text') {
+        phoneControl.setValidators(Validators.required);
+    } else {
+        phoneControl.clearValidators();
     }
-
-    save(customerForm: NgForm) {
-      console.log(customerForm.form);
-      console.log('Saved: ' + JSON.stringify(customerForm.value));
-  }
-
-    setMessage(c: AbstractControl): void {
-        this.emailMessage = '';
-        if ((c.touched || c.dirty) && c.errors) {
-            this.emailMessage = Object.keys(c.errors).map(key =>
-                this.validationMessages[key]).join(' ');
-        }
-    }
-
-    setNotification(notifyVia: string): void {
-        const phoneControl = this.customerForm.get('phone');
-        if (notifyVia === 'text') {
-            phoneControl.setValidators(Validators.required);
-        } else {
-            phoneControl.clearValidators();
-        }
-        phoneControl.updateValueAndValidity();
-    }
- }
-
+    phoneControl.updateValueAndValidity();
+}
+}
